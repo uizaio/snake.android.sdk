ba79e315857d28f64928981a224265e02327e8ac
Loitp <v.loitp@cloudhms.net>
2021-07-31 21:06:54 +0700

    update

--- snakebroadcast/src/main/java/com/uiza/sdkbroadcast/profile/VideoAttributes.kt
+++ snakebroadcast/src/main/java/com/uiza/sdkbroadcast/profile/VideoAttributes.kt
@@ -1,139 +1,102 @@
-package com.uiza.sdkbroadcast.profile;
+package com.uiza.sdkbroadcast.profile
 
-import android.content.res.Resources;
-import android.media.MediaCodecInfo;
-import android.os.Parcel;
-import android.os.Parcelable;
-
-import androidx.annotation.NonNull;
-
-import com.uiza.sdkbroadcast.util.ValidValues;
-
-import java.util.Locale;
+import android.content.res.Resources
+import android.media.MediaCodecInfo
+import android.os.Parcel
+import android.os.Parcelable
+import android.os.Parcelable.Creator
+import com.uiza.sdkbroadcast.profile.VideoSize
+import com.uiza.sdkbroadcast.util.ValidValues.check
+import java.util.*
 
 /**
  * Video Profile for Live
  * Codec H264
  */
-public class VideoAttributes implements Parcelable {
+class VideoAttributes : Parcelable {
 
-    public static final Creator<VideoAttributes> CREATOR = new Creator<VideoAttributes>() {
-        @Override
-        public VideoAttributes createFromParcel(Parcel in) {
-            return new VideoAttributes(in);
-        }
-
-        @Override
-        public VideoAttributes[] newArray(int size) {
-            return new VideoAttributes[size];
-        }
-    };
-
-    private final int MAX_BITRATE = 6000000; // 6000 kps
-
+    /**
+     * Returns the video size for the encoding process.
+     *
+     * @return The video size for the encoding process.
+     */
     /**
      * The video size for the encoding process.
      * default 1080p.
      */
-    private VideoSize size = VideoSize.FHD_1080p();
-    /**
-     * The bitrate value for the encoding process.
-     * default 6000000 (6MB)
-     */
-    private int bitRate = MAX_BITRATE;
-    /**
-     * The frame rate value for the encoding process.
-     * default 30 fps
-     */
-    private int frameRate = 30;
-
-    /**
-     * The frame interval for the encoding process.
-     * default 2 seconds.
-     */
-    private int frameInterval = 2; // sec
-
-    // dpi of your screen device.
-    private int dpi;
-
-    private VideoAttributes() {
-    }
-
-    private VideoAttributes(Parcel in) {
-        size = in.readParcelable(VideoSize.class.getClassLoader());
-        bitRate = in.readInt();
-        frameRate = in.readInt();
-        frameInterval = in.readInt();
-        dpi = in.readInt();
-    }
-
-    private VideoAttributes(VideoSize size, int frameRate, int bitRate, int frameInterval) {
-        if (!size.isValid()) {
-            throw new IllegalArgumentException("You must set size in [0,0] to [1920, 1080]");
-        }
-        ValidValues.check(frameRate, 1, 60);
-        ValidValues.check(bitRate, 1, 6000000); // max 6MB
-        ValidValues.check(frameInterval, 1, 10);
-        this.size = size;
-        this.bitRate = bitRate;
-        this.frameRate = frameRate;
-        this.frameInterval = frameInterval;
-        this.dpi = Resources.getSystem().getDisplayMetrics().densityDpi;
-    }
-
-    public static VideoAttributes FHD_1080p(int frameRate, int bitRate) {
-        return new VideoAttributes(VideoSize.FHD_1080p(), frameRate, bitRate, 2);
-    }
-
-    public static VideoAttributes FHD_1080p(int frameRate, int bitRate, int frameInterval) {
-        return new VideoAttributes(VideoSize.FHD_1080p(), frameRate, bitRate, frameInterval);
-    }
-
-    public static VideoAttributes HD_720p(int frameRate, int bitRate) {
-        return new VideoAttributes(VideoSize.HD_720p(), frameRate, bitRate, 2);
-    }
-
-    public static VideoAttributes HD_720p(int frameRate, int bitRate, int frameInterval) {
-        return new VideoAttributes(VideoSize.HD_720p(), frameRate, bitRate, frameInterval);
-    }
-
-    public static VideoAttributes SD_480p(int frameRate, int bitRate) {
-        return new VideoAttributes(VideoSize.SD_480p(), frameRate, bitRate, 2);
-    }
-
-    public static VideoAttributes SD_480p(int frameRate, int bitRate, int frameInterval) {
-        return new VideoAttributes(VideoSize.SD_480p(), frameRate, bitRate, frameInterval);
-    }
-
-    public static VideoAttributes SD_360p(int frameRate, int bitRate) {
-        return new VideoAttributes(VideoSize.SD_360p(), frameRate, bitRate, 2);
-    }
-
-    public static VideoAttributes SD_360p(int frameRate, int bitRate, int frameInterval) {
-        return new VideoAttributes(VideoSize.SD_360p(), frameRate, bitRate, frameInterval);
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeParcelable(size, flags);
-        dest.writeInt(bitRate);
-        dest.writeInt(frameRate);
-        dest.writeInt(frameInterval);
-        dest.writeInt(dpi);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
+    var size: VideoSize = VideoSize.FHD_1080p()
+        private set
     /**
      * Returns the bitrate value for the encoding process.
      *
      * @return The bitrate value for the encoding process.
      */
-    public int getBitRate() {
-        return bitRate;
+    /**
+     * The bitrate value for the encoding process.
+     * default 6000000 (6MB)
+     */
+    var bitRate = MAX_BITRATE
+        private set
+    /**
+     * Returns the frame rate value for the encoding process.
+     *
+     * @return The frame rate value for the encoding process.
+     */
+    /**
+     * The frame rate value for the encoding process.
+     * default 30 fps
+     */
+    var frameRate = 30
+        private set
+    /**
+     * @return the Frame Interval value for the encoding process.
+     */
+    /**
+     * The frame interval for the encoding process.
+     * default 2 seconds.
+     */
+    var frameInterval = 2 // sec
+        private set
+
+    /**
+     * @return dpi of your screen device.
+     */
+    // dpi of your screen device.
+    var dpi = 0
+        private set
+
+    private constructor() {}
+
+    private constructor(`in`: Parcel) {
+        size = `in`.readParcelable(VideoSize::class.java.classLoader)!!
+        bitRate = `in`.readInt()
+        frameRate = `in`.readInt()
+        frameInterval = `in`.readInt()
+        dpi = `in`.readInt()
+    }
+
+    private constructor(size: VideoSize, frameRate: Int, bitRate: Int, frameInterval: Int) {
+        require(size.isValid) { "You must set size in [0,0] to [1920, 1080]" }
+        check(value = frameRate, min = 1, max = 60)
+        check(value = bitRate, min = 1, max = 6000000) // max 6MB
+        check(value = frameInterval, min = 1, max = 10)
+        this.size = size
+        this.bitRate = bitRate
+        this.frameRate = frameRate
+        this.frameInterval = frameInterval
+        dpi = Resources.getSystem().displayMetrics.densityDpi
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeParcelable(size, flags)
+        dest.writeInt(bitRate)
+        dest.writeInt(frameRate)
+        dest.writeInt(frameInterval)
+        dest.writeInt(dpi)
+    }
+
+    override fun describeContents(): Int {
+        return 0
     }
 
     /**
@@ -143,18 +106,9 @@ public class VideoAttributes implements Parcelable {
      * @param bitRate The bitrate value for the encoding process.
      * @return this instance
      */
-    public VideoAttributes setBitRate(int bitRate) {
-        this.bitRate = bitRate;
-        return this;
-    }
-
-    /**
-     * Returns the video size for the encoding process.
-     *
-     * @return The video size for the encoding process.
-     */
-    public VideoSize getSize() {
-        return size;
+    fun setBitRate(bitRate: Int): VideoAttributes {
+        this.bitRate = bitRate
+        return this
     }
 
     /**
@@ -164,18 +118,9 @@ public class VideoAttributes implements Parcelable {
      * @param size he video size for the encoding process.
      * @return this instance
      */
-    public VideoAttributes setSize(VideoSize size) {
-        this.size = size;
-        return this;
-    }
-
-    /**
-     * Returns the frame rate value for the encoding process.
-     *
-     * @return The frame rate value for the encoding process.
-     */
-    public int getFrameRate() {
-        return frameRate;
+    fun setSize(size: VideoSize): VideoAttributes {
+        this.size = size
+        return this
     }
 
     /**
@@ -185,37 +130,20 @@ public class VideoAttributes implements Parcelable {
      * @param frameRate The frame rate value for the encoding process.
      * @return this instance
      */
-    public VideoAttributes setFrameRate(int frameRate) {
-        this.frameRate = frameRate;
-        return this;
+    fun setFrameRate(frameRate: Int): VideoAttributes {
+        this.frameRate = frameRate
+        return this
     }
 
-    /**
-     * @return the Frame Interval value for the encoding process.
-     */
-    public int getFrameInterval() {
-        return frameInterval;
+    fun setFrameInterval(frameInterval: Int): VideoAttributes {
+        this.frameInterval = frameInterval
+        return this
     }
 
-    public VideoAttributes setFrameInterval(Integer frameInterval) {
-        this.frameInterval = frameInterval;
-        return this;
-    }
-
-    public int getAVCProfile() {
-        return size.isHighResolution() ? MediaCodecInfo.CodecProfileLevel.AVCProfileHigh : MediaCodecInfo.CodecProfileLevel.AVCProfileMain;
-    }
-
-    public int getAVCProfileLevel() {
-        return size.isHighResolution() ? MediaCodecInfo.CodecProfileLevel.AVCLevel4 : MediaCodecInfo.CodecProfileLevel.AVCLevel31;
-    }
-
-    /**
-     * @return dpi of your screen device.
-     */
-    public int getDpi() {
-        return dpi;
-    }
+    val aVCProfile: Int
+        get() = if (size.isHighResolution) MediaCodecInfo.CodecProfileLevel.AVCProfileHigh else MediaCodecInfo.CodecProfileLevel.AVCProfileMain
+    val aVCProfileLevel: Int
+        get() = if (size.isHighResolution) MediaCodecInfo.CodecProfileLevel.AVCLevel4 else MediaCodecInfo.CodecProfileLevel.AVCLevel31
 
     /**
      * Sets dpi of your screen device.
@@ -223,16 +151,72 @@ public class VideoAttributes implements Parcelable {
      * @param dpi dpi of your screen device.
      * @return this instance
      */
-    public VideoAttributes setDpi(int dpi) {
-        this.dpi = dpi;
-        return this;
+    fun setDpi(dpi: Int): VideoAttributes {
+        this.dpi = dpi
+        return this
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return String.format(Locale.getDefault(),
-                "VideoAttributes (res: %s, fps: %d, bitrate: %d, iFrameInterval: %d, dpi: %d)",
-                size.toString(), frameRate, bitRate, frameInterval, dpi);
+    override fun toString(): String {
+        return String.format(
+            Locale.getDefault(),
+            "VideoAttributes (res: %s, fps: %d, bitrate: %d, iFrameInterval: %d, dpi: %d)",
+            size.toString(), frameRate, bitRate, frameInterval, dpi
+        )
+    }
+
+    companion object {
+
+        private const val MAX_BITRATE = 6000000 // 6000 kps
+
+        @JvmField
+        val CREATOR: Creator<VideoAttributes?> = object : Creator<VideoAttributes?> {
+            override fun createFromParcel(`in`: Parcel): VideoAttributes {
+                return VideoAttributes(`in`)
+            }
+
+            override fun newArray(size: Int): Array<VideoAttributes?> {
+                return arrayOfNulls(size)
+            }
+        }
+
+        @JvmStatic
+        fun FHD_1080p(frameRate: Int, bitRate: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.FHD_1080p(), frameRate, bitRate, 2)
+        }
+
+        @JvmStatic
+        fun FHD_1080p(frameRate: Int, bitRate: Int, frameInterval: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.FHD_1080p(), frameRate, bitRate, frameInterval)
+        }
+
+        @JvmStatic
+        fun HD_720p(frameRate: Int, bitRate: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.HD_720p(), frameRate, bitRate, 2)
+        }
+
+        @JvmStatic
+        fun HD_720p(frameRate: Int, bitRate: Int, frameInterval: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.HD_720p(), frameRate, bitRate, frameInterval)
+        }
+
+        @JvmStatic
+        fun SD_480p(frameRate: Int, bitRate: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.SD_480p(), frameRate, bitRate, 2)
+        }
+
+        @JvmStatic
+        fun SD_480p(frameRate: Int, bitRate: Int, frameInterval: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.SD_480p(), frameRate, bitRate, frameInterval)
+        }
+
+        @JvmStatic
+        fun SD_360p(frameRate: Int, bitRate: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.SD_360p(), frameRate, bitRate, 2)
+        }
+
+        @JvmStatic
+        fun SD_360p(frameRate: Int, bitRate: Int, frameInterval: Int): VideoAttributes {
+            return VideoAttributes(VideoSize.SD_360p(), frameRate, bitRate, frameInterval)
+        }
     }
 }
